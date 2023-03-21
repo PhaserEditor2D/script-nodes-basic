d6ecf17e5432153cb8c4b0504783083d769bca89
PhaserEditor2D <developers@phasereditor2d.com>
2023-03-21 03:31:36 -0400

    Adds scripts.

---
+++ scripts/OnSceneAwakeScript.ts
@@ -0,0 +1,34 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import ScriptNode from "./ScriptNode";
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class OnSceneAwakeScript extends ScriptNode {
+
+	constructor(parent: ScriptNode | Phaser.GameObjects.GameObject | Phaser.Scene) {
+		super(parent);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	protected override awake(): void {
+		
+		console.log("on scene awake script")
+		this.executeChildren();
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
